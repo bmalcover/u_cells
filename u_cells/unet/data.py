5598eee36eea851b718b210540fc65207359ec76
Miquel Miró Nicolau <miquelca32@gmail.com>
2021-06-11 18:07:23 +0200

    Added the ability to normalize the number of regions with mean = 0 and std = 1

--- u_cells/unet/data.py
+++ u_cells/unet/data.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-import os
 from typing import Tuple, Union
 from enum import Enum
+import os
 
 import numpy as np
 import json
-from icecream import ic
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import utils as KU
@@ -66,7 +65,7 @@ class DataGenerator(KU.Sequence):
     def __init__(self, steps: int, batch_size: int, path: str, image_folder: str, mask_folder: str,
                  aug_dict: dict, dcd_mode: Union[DecodeMode, None], img_color_mode: str,
                  mask_color_mode: str, target_size: Tuple[int, int], seed: int = 1,
-                 do_regression: Union[str, None] = None):
+                 do_regression: Union[str, None] = None, normalize_region_data: bool = False):
         self.__steps = steps
         self.__path = path
         self.__decode_mode = dcd_mode
@@ -95,13 +94,49 @@ class DataGenerator(KU.Sequence):
         )
 
         if do_regression is not None:
-            self.__regression_data = json.load(open(do_regression))
+            self.__regression_data = self.__get_regions_info(do_regression, normalize_region_data)
         else:
             self.__regression_data = None
 
+        self.__stadistic = []
         self.__generator = self.__get_merged_info()
 
+    def __get_regions_info(self, path: str, normalize: bool = False):
+        """ Gets the information of the regions.
+
+        The information is stored with VIA 2.0 format. The regions are saved as a dictionary for each image, the key is
+        "regions". This key is a list of regions. We are only interested on the number of regions
+
+        TODO: Add the ability to return also the region depending on a parameter
+
+        Args:
+            path (str): String containing the information of the regions.
+
+        Returns:
+
+        """
+        info = json.load(open(path))
+
+        mean = 0
+        std = 1
+
+        if normalize:
+            mean_std = np.array([len(v) for v in info.values()])
+            mean = mean_std.mean()
+            std = mean_std.std()
+
+        self.__stadistic = [mean, std]
+
+        info = {k: (len(v["regions"]) - mean) / std for k, v in info.items()}
+
+        return info
+
     def __get_merged_info(self):
+        """ Yields the information from the dataset.
+
+        Returns:
+
+        """
         for img, mask in zip(self.__image_generator, self.__mask_generator):
             img = img / 255
 
@@ -131,11 +166,23 @@ class DataGenerator(KU.Sequence):
                 for filename in batch_filenames:
                     region_key = os.path.split(filename)[-1].split(".")[0]
 
-                    n_cells.append(len(list(self.__regression_data.values())[int(region_key)]["regions"]))
+                    n_cells.append(len(list(self.__regression_data)[int(region_key)]))
 
                 n_cells = np.array(n_cells)
 
                 yield img, {"img_out": mask, 'regressor_output': n_cells}
+
+    @property
+    def mean(self) -> Union[int, float]:
+        assert len(self.__stadistic) == 2
+
+        return self.__stadistic[0]
+
+    @property
+    def std(self) -> Union[int, float]:
+        assert len(self.__stadistic) == 2
+
+        return self.__stadistic[1]
 
     def __len__(self):
         return self.__steps
