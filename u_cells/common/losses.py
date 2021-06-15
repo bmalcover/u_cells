8cef71604188d8849d1f81a44d089ca4fb6844e1
miquelmn <miquelca32@gmail.com>
2021-06-15 09:37:50 +0200

    Added own losses, and moved all of them into common package

--- u_cells/common/losses.py
+++ u_cells/common/losses.py
@@ -6,6 +6,21 @@ import tensorflow.keras.backend as K
 import tensorflow as tf
 
 
+def onw_dice_coef(y_true, y_pred, smooth=1):
+    """
+    Dice = (2*|X & Y|)/ (|X|+ |Y|)
+         =  2*sum(|A*B|)/(sum(A^2)+sum(B^2))
+    ref: https://arxiv.org/pdf/1606.04797v1.pdf
+    """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    return (2. * intersection + smooth) / (
+                K.sum(K.square(y_true), -1) + K.sum(K.square(y_pred), -1) + smooth)
+
+
+def own_dice_coef_loss(y_true, y_pred):
+    return 1 - onw_dice_coef(y_true, y_pred)
+
+
 def multiclass_weighted_dice_loss(class_weights: Union[list, np.ndarray, tf.Tensor]) -> Callable[
     [tf.Tensor, tf.Tensor], tf.Tensor]:
     """ Weighted Dice loss.
